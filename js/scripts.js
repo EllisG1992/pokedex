9bbf552dd23fc620f39cd45fd5462b324b79f374
EllisG1992 <garcia.a.ellis@gmail.com>
2021-04-05 14:39:25 -0700

    insert line break after each pokemeon printout

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,5 @@ let pokemonList = [
 
 for (let i = 0; i < pokemonList.length; i++){
   document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+  document.write('<br><br>');
 }
