879b087edc8057b6c2906b1b86aa54b1f7a503a8
EllisG1992 <garcia.a.ellis@gmail.com>
2021-04-04 19:56:52 -0700

    ensured types in objects in pokemonList are all arrays of strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
   {name: 'Charizard', height: 1.7, types: ['fire','flying']},
-  {name: 'Blastoise', height: 1.6, types: 'water'},
+  {name: 'Blastoise', height: 1.6, types: ['water']},
   {name: 'Zapdos', height: 1.6, types: ['electric','flying']}
 ];
