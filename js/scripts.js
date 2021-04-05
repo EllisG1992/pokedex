6e3efbaa200991781c7da92c11cb4d3e8a1f9a4d
EllisG1992 <garcia.a.ellis@gmail.com>
2021-04-04 19:52:58 -0700

    add 3 objects to pokemonlist

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = [];
+let pokemonList = [
+  {name: 'Charizard', height: 1.7, types: ['fire','flying']},
+  {name: 'Blastoise', height: 1.6, types: 'water'},
+  {name: 'Zapdos', height: 1.6, types: ['electric','flying']}
+];
