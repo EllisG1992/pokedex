fb860a9c0591d8001947644299e5065b47974bb1
EllisG1992 <garcia.a.ellis@gmail.com>
2021-04-05 14:32:53 -0700

    create for loop to present all pokemon in pokemon list with height next to name

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,7 @@ let pokemonList = [
   {name: 'Blastoise', height: 1.6, types: ['water']},
   {name: 'Zapdos', height: 1.6, types: ['electric','flying']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++){
+  document.write(pokemonList[i].name + ' height: (' + pokemonList[i].height + ')');
+}
