0d02def3414b5e6a2d76ea2b89b3f6e753721916
EllisG1992 <garcia.a.ellis@gmail.com>
2021-04-06 13:20:14 -0700

    reformtted for loop and made code cleaner by defining new variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
 let pokemonList = [
-  {name: 'Bulbasaur', height: 0.7, types: ['grass','poison']},
-  {name: 'Charmeleon', height: 1.1, types: ['fire']},
-  {name: 'Blastoise', height: 1.6, types: ['water']}
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['grass','poison']
+  },
+  {
+    name: 'Charmeleon',
+    height: 1.1,
+    types: ['fire']
+  },
+  {
+    name: 'Blastoise',
+    height: 1.6,
+    types: ['water']
+  }
 ];
 
 for (let i = 0; i < pokemonList.length; i++){
-  if (pokemonList[i].height > 1.5){
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' +
-  ' - Wow, that\'s big!');
-    document.write('<br><br>');
-  } else if (pokemonList[i].height < 1.5){
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
-    document.write('<br><br>');
+  let pokemon = pokemonList[i];
+
+  let message = `${pokemon.name} (height: ${pokemon.height})`;
+
+  if(pokemon.height > 1.5){
+    message = message + ' Wow, that\'s big!';
   }
+
+  document.write(message);
+  document.write('<br><br>');
 }
