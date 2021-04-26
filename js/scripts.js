522731e5f438f481bf76d5927df98e62ab8b6158
EllisG1992 <garcia.a.ellis@gmail.com>
2021-04-26 13:33:55 -0700

    exercise 1.8 - created modal showing pokemon name, height and image url when pokemon is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,67 @@
 let pokemonRepository = (function (){
+  let modalContainer = document.querySelector('#modal-container');
+  //let imageContainer = document.querySelector('#image-container');
+
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+  function showModal(title, text, img){
+    //clear all existing modal content
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    //add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    let myImage = document.createElement('img');
+    myImage.src = img;
+    myImage.classList.add('image-element');
+
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(myImage);
+
+    modalContainer.appendChild(modal);
+    modalContainer.appendChild(myImage);
+
+    //imageContainer.appendChild(myImage);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal(){
+    modalContainer.classList.remove('is-visible');
+  }
+
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is the modal content', );
+  });
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer){
+      hideModal();
+    }
+  });
 
   function getAll(){
     return pokemonList;
@@ -56,7 +117,9 @@ let pokemonRepository = (function (){
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function () {
       console.log(item);
+      showModal(item.name, 'height: ' + item.height, item.imageUrl);
     });
+
   }
 
   return{
